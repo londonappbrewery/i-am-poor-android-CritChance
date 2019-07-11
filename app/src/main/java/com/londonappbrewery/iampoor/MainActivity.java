ec9b373a7b8ea78a8862184dc6f08998f69a3171
chanc <chance.bloom7@gmail.com>
2019-07-10 18:46:01 -0700

    all done

--- app/src/main/java/com/londonappbrewery/iampoor/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/iampoor/MainActivity.java
@@ -2,7 +2,9 @@ package com.londonappbrewery.iampoor;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
